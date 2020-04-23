560ebe86527d2d1220a7717b7a4b63cd1ab5c79e
lyno4ka <yuliyamasterenko@gmail.com>
2020-04-23 04:07:16 +0300

    init

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
-export const environment = {
-  production: true
+import {Environment} from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCONl7Mp4-eVVs6lnRRnvSNMYFR9kBZvo0',
+  fbDbUrl: 'https://angular-blog-a59c7.firebaseio.com'
 };
